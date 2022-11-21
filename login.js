cfcd4aa4056dceb0f70c42065c6e8d1034883f6d
aleena20102016 <88888878+aleena20102016@users.noreply.github.com>
2022-11-21 20:37:22 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,5 +5,5 @@ function Login()
     player_name = document.getElementById("playername").value;
     localStorage.setItem("player_name", player_name);
 
-    window.location = "gamepage.js";
+    window.location = "gamepage.html";
 }
